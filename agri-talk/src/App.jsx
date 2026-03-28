d43724030509acb7d87bcf90337f12d970486b80
saito0120 <132254371+saito0120@users.noreply.github.com>
2026-03-28 02:21:53 +0000

    App.jsx変更

---
+++ agri-talk/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🌱 AgriTalk</h1>
+      <p>農業をもっとスマートに</p>
+      <button>ログイン</button>
+    </div>
+  );
+}
+
+export default App;
